9333f2be882f8fb4aa808493f298c5bcd032063f
abe <abrahim.gaber@gmail.com>
2012-12-26 18:40:58 -0500

    beanutil test

---
+++ src/main/java/com/acme/sandbox/TestObject.java
@@ -0,0 +1,84 @@
+package com.acme.sandbox;
+
+import com.google.common.base.Objects;
+
+public class TestObject {
+  public String prop1;
+  public long prop2;
+  private int prop3;
+
+  public TestObject() {
+    // pass
+  }
+
+  public TestObject(String prop1, long prop2, int prop3) {
+    this.prop1 = prop1;
+    this.prop2 = prop2;
+    this.prop3 = prop3;
+  }
+
+  public String getProp1() {
+    return prop1;
+  }
+
+  public void setProp1(String prop1) {
+    this.prop1 = prop1;
+  }
+
+  public long getProp2() {
+    return prop2;
+  }
+
+  public void setProp2(long prop2) {
+    this.prop2 = prop2;
+  }
+
+  public int getProp3() {
+    return prop3;
+  }
+
+  public void setProp3(int prop3) {
+    this.prop3 = prop3;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((prop1 == null) ? 0 : prop1.hashCode());
+    result = prime * result + (int) (prop2 ^ (prop2 >>> 32));
+    result = prime * result + prop3;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof TestObject))
+      return false;
+    TestObject other = (TestObject) obj;
+    if (prop1 == null) {
+      if (other.prop1 != null)
+        return false;
+    } else if (!prop1.equals(other.prop1))
+      return false;
+    if (prop2 != other.prop2)
+      return false;
+    if (prop3 != other.prop3)
+      return false;
+    return true;
+  }
+
+
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(getClass())
+        .add("prop1", prop1)
+        .add("prop2", prop2)
+        .add("prop3", prop3)
+        .toString();
+  }
+}
--- src/test/java/com/acme/sandbox/ReflectionTest.java
+++ src/test/java/com/acme/sandbox/ReflectionTest.java
@@ -1,17 +1,36 @@
 package com.acme.sandbox;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.util.Map;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.junit.Test;
+
 
 public class ReflectionTest {
 
   @Test
   public void nameShouldIncludePackage() throws Exception {
     assertEquals("com.acme.sandbox.ReflectionTest", getClass().getName());
+  }
+
+  @Test
+  public void shouldSetPropertiesWithBeanUtils() throws Exception {
+    TestObject bean = new TestObject();
+
+    @SuppressWarnings("unchecked")
+    Map<String, Object> properties = BeanUtils.describe(bean);
+    properties.put("prop1", "the prop");
+    properties.put("prop2", 100);
+    properties.put("prop3", 400);
+    BeanUtils.populate(bean, properties);
+    assertEquals(new TestObject("the prop", 100, 400), bean);
+
+    // The conclusion of this test is that it has to be a public class and it
+    // needs getters and setters.
   }
 }
 
