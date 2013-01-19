a5cfbf4f25a7741dc44b3b05912fd239857ca974
abe <abrahim.gaber@gmail.com>
2013-01-19 01:25:25 -0500

    white space

--- src/test/java/com/acme/sandbox/SetTheoryTest.java
+++ src/test/java/com/acme/sandbox/SetTheoryTest.java
@@ -14,7 +14,7 @@ public class SetTheoryTest {
   public void union() {
     Collection<Integer> a = Lists.newArrayList(1, 2, 3, 3);
     Collection<Integer> b = Lists.newArrayList(2, 3, 1, 4);
-    Collection<Integer>result = SetTheory.union(a, b);
+    Collection<Integer> result = SetTheory.union(a, b);
     assertArrayEquals(
         new Integer[] { 1, 2, 3, 4 },
         result.toArray());
@@ -24,7 +24,7 @@ public class SetTheoryTest {
   public void unionStrings() {
     Collection<String> a = Lists.newArrayList("a", "b", "c");
     Collection<String> b = Lists.newArrayList("d", "e", "a");
-    Collection<String>result = SetTheory.union(a, b);
+    Collection<String> result = SetTheory.union(a, b);
     assertArrayEquals(
         new String[] { "d", "e", "b", "c", "a" },
         result.toArray());
@@ -34,7 +34,7 @@ public class SetTheoryTest {
   public void intersection() {
     Collection<Integer> a = Lists.newArrayList(1, 2, 3, 3, 3);
     Collection<Integer> b = Lists.newArrayList(2, 3, 1, 4, 3);
-    Collection<Integer>result = SetTheory.intersection(a, b);
+    Collection<Integer> result = SetTheory.intersection(a, b);
     assertArrayEquals(
         new Integer[] { 1, 2, 3 },
         result.toArray());
