dfa11a9c01fa8b5afda727b16b30956026ef979e
abe <abrahim.gaber@gmail.com>
2013-01-20 14:07:33 -0500

    minor changes to settheory stuff

--- src/main/java/com/acme/sandbox/SetTheory.java
+++ src/main/java/com/acme/sandbox/SetTheory.java
@@ -1,6 +1,5 @@
 package com.acme.sandbox;
 
-import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Set;
 
@@ -10,37 +9,24 @@ import com.google.common.collect.Sets;
 public final class SetTheory {
 
   @SafeVarargs
-  public static <T extends Comparable<T>> Collection<T> intersection(Collection<T>... sets) {
+  public static <T extends Comparable<T>> Set<T> intersection(Set<T>... sets) {
     if (sets.length == 0) {
       return ImmutableSet.of();
     }
 
     Set<T> result = Sets.newTreeSet(sets[0]);
-    for (int i = 1; i < sets.length; i++) {
-      Set<T> set = ImmutableSet.copyOf(sets[i]);
+    for (int i = 1; i < sets.length; i++)
       for (T value : ImmutableSet.copyOf(result))
-        if (!set.contains(value))
+        if (!sets[i].contains(value))
           result.remove(value);
-    }
     return ImmutableSet.copyOf(result);
   }
 
   @SafeVarargs
-  public static <T extends Comparable<T>> Collection<T> union(Collection<T>... sets) {
+  public static <T extends Comparable<T>> Set<T> union(Set<T>... sets) {
     Set<T> result = Sets.newTreeSet();
     for(Collection<T> set : sets)
       result.addAll(set);
     return ImmutableSet.copyOf(result);
-  }
-
-  @SafeVarargs
-  public static <T extends Comparable<T>> T[] union(Class<T> type, T[]... sets) {
-    Set<T> result = Sets.newTreeSet();
-    for (T[] set : sets)
-      for (T o : set)
-        result.add(o);
-    @SuppressWarnings("unchecked")
-    T[] array = (T[]) Array.newInstance(type, result.size());
-    return result.toArray(array);
   }
 }
--- src/test/java/com/acme/sandbox/SetTheoryTest.java
+++ src/test/java/com/acme/sandbox/SetTheoryTest.java
@@ -2,19 +2,19 @@ package com.acme.sandbox;
 
 import static org.junit.Assert.assertArrayEquals;
 
-import java.util.Collection;
+import java.util.Set;
 
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 public class SetTheoryTest {
   @Test
   public void intersection() throws Exception {
-    Collection<Integer> a = Lists.newArrayList(1, 2, 3, 3, 3, 4);
-    Collection<Integer> b = Lists.newArrayList(2, 3, 1, 4, 3, 7);
-    Collection<Integer> c = Lists.newArrayList(1, 4, 7, 7);
-    Collection<Integer> result = SetTheory.intersection(a, b, c);
+    Set<Integer> a = Sets.newHashSet(1, 2, 3, 3, 3, 4);
+    Set<Integer> b = Sets.newHashSet(2, 3, 1, 4, 3, 7);
+    Set<Integer> c = Sets.newHashSet(1, 4, 7, 7);
+    Set<Integer> result = SetTheory.intersection(a, b, c);
     assertArrayEquals(
         new Integer[] { 1, 4 },
         result.toArray());
@@ -27,8 +27,8 @@ public class SetTheoryTest {
         new String[] { },
         SetTheory.<String>intersection().toArray());
 
-    Collection<Integer> a = Lists.newArrayList(1, 2, 3, 3, 3, 4);
-    Collection<Integer> result = SetTheory.intersection(a);
+    Set<Integer> a = Sets.newHashSet(1, 2, 3, 3, 3, 4);
+    Set<Integer> result = SetTheory.intersection(a);
     assertArrayEquals(
         "Should handle intersection of one set.",
         new Integer[] { 1, 2, 3, 4 },
@@ -37,10 +37,10 @@ public class SetTheoryTest {
 
   @Test
   public void union() throws Exception {
-    Collection<Integer> a = Lists.newArrayList(1, 2, 3, 3);
-    Collection<Integer> b = Lists.newArrayList(2, 3, 1, 4);
-    Collection<Integer> c = Lists.newArrayList(4, 5, 5, 5, 7);
-    Collection<Integer> result = SetTheory.union(a, b, c);
+    Set<Integer> a = Sets.newHashSet(1, 2, 3, 3);
+    Set<Integer> b = Sets.newHashSet(2, 3, 1, 4);
+    Set<Integer> c = Sets.newHashSet(4, 5, 5, 5, 7);
+    Set<Integer> result = SetTheory.union(a, b, c);
     assertArrayEquals(
         new Integer[] { 1, 2, 3, 4, 5, 7 },
         result.toArray());
@@ -48,9 +48,9 @@ public class SetTheoryTest {
 
   @Test
   public void unionStrings() throws Exception {
-    Collection<String> a = Lists.newArrayList("a", "b", "c");
-    Collection<String> b = Lists.newArrayList("d", "e", "a");
-    Collection<String> result = SetTheory.union(a, b);
+    Set<String> a = Sets.newHashSet("a", "b", "c");
+    Set<String> b = Sets.newHashSet("d", "e", "a");
+    Set<String> result = SetTheory.union(a, b);
     assertArrayEquals(
         new String[] { "a", "b", "c", "d", "e" },
         result.toArray());
@@ -63,21 +63,11 @@ public class SetTheoryTest {
         new String[] { },
         SetTheory.<String>union().toArray());
 
-    Collection<String> a = Lists.newArrayList("a", "b", "c");
-    Collection<String> result = SetTheory.union(a);
+    Set<String> a = Sets.newHashSet("a", "b", "c");
+    Set<String> result = SetTheory.union(a);
     assertArrayEquals(
         "Should handle union of one set.",
         new String[] { "a", "b", "c" },
         result.toArray());
-  }
-
-  @Test
-  public void unionArrays() throws Exception {
-    Integer[] a = new Integer[] { 1, 2, 3, 4, 20 };
-    Integer[] b = new Integer[] { 1, 4, 4 };
-    Integer[] result = SetTheory.union(Integer.class, a, b);
-    assertArrayEquals(
-        new Integer[] { 1, 2, 3, 4, 20 },
-        result);
   }
 }
