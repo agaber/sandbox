bb38131d3fe639d0fa29a035129db1f47ceb7c62
abe <abrahim.gaber@gmail.com>
2013-01-22 01:44:07 -0500

    extends Map, renamed to Dictionary

--- src/main/java/com/acme/sandbox/Dictionary.java
+++ src/main/java/com/acme/sandbox/Dictionary.java
@@ -3,11 +3,20 @@ package com.acme.sandbox;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.google.common.collect.Lists;
 
-public class Hashtable<K, V> {
+/**
+ * My own implementation of {@link Map}, for fun.
+ *
+ * @param <K> key type
+ * @param <V> value type
+ */
+public class Dictionary<K, V> implements Map<K, V> {
   private static class Node<K, V> {
     public final K key;
     public final V value;
@@ -27,17 +36,41 @@ public class Hashtable<K, V> {
   private int size;
   private Object[] values;
 
-  public Hashtable() {
+  public Dictionary() {
     this(100);
   }
 
-  public Hashtable(int initialCapacity) {
+  public Dictionary(int initialCapacity) {
     checkArgument(initialCapacity > 0, "the initialCapacity must be greater than zero");
     this.capacity = initialCapacity;
     this.values = new Object[capacity];
   }
 
-  public V get(final K key) {
+  @Override
+  public Set<Map.Entry<K, V>> entrySet() {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public void clear() {
+    // TODO
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    // TODO
+    return false;
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    // TODO
+    return false;
+  }
+
+  @Override
+  public V get(Object key) {
     List<Node<K, V>> bucket = getBucket(key);
 
     for (Node<K, V> node : bucket)
@@ -47,21 +80,20 @@ public class Hashtable<K, V> {
     return null;
   }
 
-  // TODO
-  public boolean containsKey(K k) {
-    return false;
-  }
-
-  // TODO
-  public boolean containsValue(V value) {
-    return false;
-  }
-
+  @Override
   public boolean isEmpty() {
     return size == 0;
   }
 
-  public void put(final K key, final V value) {
+
+  @Override
+  public Set<K> keySet() {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public V put(final K key, final V value) {
     List<Node<K, V>> bucket = getBucket(key);
     Node<K, V> node = new Node<K, V>(key, value);
     if (!bucket.contains(node)) {
@@ -71,9 +103,17 @@ public class Hashtable<K, V> {
       bucket.remove(node);
       bucket.add(node);
     }
+
+    return value;
   }
 
-  public V remove(K key) {
+  @Override
+  public void putAll(Map<? extends K, ? extends V> m) {
+    // TODO
+  }
+
+  @Override
+  public V remove(Object key) {
     List<Node<K, V>> bucket = getBucket(key);
     Node<K, V> found = null;
 
@@ -92,12 +132,19 @@ public class Hashtable<K, V> {
     return null;
   }
 
+  @Override
   public int size() {
     return size;
   }
 
+  @Override
+  public Collection<V> values() {
+    // TODO
+    return null;
+  }
+
   @SuppressWarnings("unchecked")
-  private List<Node<K, V>> getBucket(final K key) {
+  private List<Node<K, V>> getBucket(final Object key) {
     checkNotNull(key, "key must not be null");
     int index = key.hashCode() % capacity;
     List<Node<K, V>> bucket = (List<Node<K, V>>) values[index];
--- src/test/java/com/acme/sandbox/DictionaryTest.java
+++ src/test/java/com/acme/sandbox/DictionaryTest.java
@@ -5,13 +5,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Map;
+
 import org.junit.Test;
 
-public class HashtableTest {
+public class DictionaryTest {
 
   @Test
   public void itShouldPutAndGet() throws Exception {
-    Hashtable<String, String> table = new Hashtable<String, String>();
+    Map<String, String> table = new Dictionary<String, String>();
     table.put("a", "abc");
     table.put("b", "bcd");
     table.put("c", "cde");
@@ -26,9 +28,28 @@ public class HashtableTest {
     assertNull(table.get("x"));
   }
 
+
+  @Test
+  public void itShouldReturnTheSizeAndIsEmpty() throws Exception {
+    Map<String, String> table = new Dictionary<String, String>();
+    assertTrue(table.isEmpty());
+
+    table.put("a", "abc");
+    assertEquals(1, table.size());
+    assertFalse(table.isEmpty());
+
+    table.put("b", "bcd");
+    assertEquals(2, table.size());
+    assertFalse(table.isEmpty());
+
+    table.put("c", "cde");
+    assertEquals(3, table.size());
+    assertFalse(table.isEmpty());
+  }
+
   @Test
   public void itShouldRemove() throws Exception {
-    Hashtable<Integer, String> table = new Hashtable<Integer, String>();
+    Map<Integer, String> table = new Dictionary<Integer, String>();
     table.put(1, "a");
     table.put(2, "b");
 
@@ -47,23 +68,5 @@ public class HashtableTest {
     assertNull(removed3);
     assertEquals(0, table.size());
     assertTrue(table.isEmpty());
-  }
-
-  @Test
-  public void itShouldReturnTheSizeAndIsEmpty() throws Exception {
-    Hashtable<String, String> table = new Hashtable<String, String>();
-    assertTrue(table.isEmpty());
-
-    table.put("a", "abc");
-    assertEquals(1, table.size());
-    assertFalse(table.isEmpty());
-
-    table.put("b", "bcd");
-    assertEquals(2, table.size());
-    assertFalse(table.isEmpty());
-
-    table.put("c", "cde");
-    assertEquals(3, table.size());
-    assertFalse(table.isEmpty());
   }
 }
